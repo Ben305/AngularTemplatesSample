0ae66c46babbdf8be558eca8f4b35633bc60e7c5
Benedikt Huß <ben305@users.noreply.github.com>
2016-07-22 08:05:21 +0200

    Add reproduction for failing ng-include

--- app/client/main.js
+++ app/client/main.js
@@ -2,7 +2,9 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 
-import appModule from '/imports/client/app'
+import appModule from '/imports/client/app';
+
+import '/imports/client/include.html';
 
 angular.module('sampleApp', [
   angularMeteor,
